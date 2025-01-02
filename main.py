7eee3dd69f1f34b04e324ada4bc6354f0f35ce5f
ihladni <ivan.hladni@gmail.com>
2025-01-02 23:47:36 +0100

    B: clock.tick added

--- main.py
+++ main.py
@@ -9,14 +9,23 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    # Clock
+    clock = pygame.time.Clock()
+    dt = 0
+
     #############################################
     while True:
+        # Event and Player handling
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
+        # Render
         pygame.Surface.fill(screen, (0, 0, 0))
         pygame.display.flip()
+
+        # Time 
+        dt = clock.tick(60) / 1000
     #############################################
 
 if __name__ == "__main__":
